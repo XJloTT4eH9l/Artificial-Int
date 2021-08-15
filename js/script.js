58d221e05f1e1437cb8d4803fa94a200b9e9f002
Andrew <73196050+XJloTT4eH9l@users.noreply.github.com>
2021-08-16 00:28:50 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000
+    });
+});
